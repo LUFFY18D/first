efade74bf39a9d5bb0da44a1e571c0dc68bb7fa3
LUFFY18D <1366541915@qq.com>
2019-04-28 19:35:07 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int a[5] = { 0,1, 2, 3, 4 };
+
+void backtrack(int i)
+{
+	int j, t;
+	if (i > 4)
+	{
+		if (a[0] * a[1] - a[2] * a[3] + a[4] == 1)
+		{
+			printf("%d * %d - %d * %d + %d = 1\n", a[0],a[1],a[2],a[3],a[4]);
+		}
+	}
+	else
+	{
+		for (j = i; j <= 4; j++)
+		{
+			t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+			
+			backtrack(i + 1);
+
+			t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+
+		}
+	}
+}
+
+int main()
+{
+	backtrack(0);
+	return 0;
+}
